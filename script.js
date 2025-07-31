199996407808fb14ff8624226339f11a1f57d470
Hai Dang Nguyen <haidangnguyenus@gmail.com>
2025-07-30 20:21:27 -0700

    feat: complete rock paper scissors game logic and simple ui using prompt

---
+++ script.js
@@ -0,0 +1,50 @@
+let humanScore;
+let computerScore;
+
+function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"];
+    randomInput = Math.floor(Math.random() * choices.length);
+    return choices[randomInput];
+}
+function getHumanChoice() {
+    return prompt("Rock, paper, or scissors?");
+}
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    const matchups = new Map([
+        ["rock,rock", 0],
+        ["rock,paper", -1],
+        ["rock,scissors", 1],
+        ["paper,rock", 1],
+        ["paper,paper", 0],
+        ["paper,scissors", -1],
+        ["scissors,scissors", 0],
+        ["scissors,rock", -1],
+        ["scissors,paper", 1],
+    ]);
+
+    if (matchups.get(`${humanChoice},${computerChoice}`) == 1) {
+        humanScore++;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    } else if (matchups.get(`${humanChoice},${computerChoice}`) == 0) {
+        console.log(`Tie! Y'all both chose ${humanChoice}`);
+    } else {
+        computerScore++;
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+}
+function playGame() {
+    humanScore = 0;
+    computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+    if (humanScore < computerScore) {
+        console.log("You Lose!");
+    } else if (humanScore > computerScore) {
+        console.log("You win!");
+    } else {
+        console.log("Tie!");
+    }
+}
+playGame();
