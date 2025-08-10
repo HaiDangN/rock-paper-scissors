b651a71f5467c93b48460482ce7d9b3f02ee418c
Hai Dang Nguyen <haidangnguyenus@gmail.com>
2025-08-10 04:07:46 -0400

    refactor: move constant vars to top-level scope

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-let humanScore;
-let computerScore;
+const POINTS_TO_WIN = 5;
+const matchups = new Map([
+  ["rock,rock", 0],
+  ["rock,paper", -1],
+  ["rock,scissors", 1],
+  ["paper,rock", 1],
+  ["paper,paper", 0],
+  ["paper,scissors", -1],
+  ["scissors,scissors", 0],
+  ["scissors,rock", -1],
+  ["scissors,paper", 1],
+]);
+const rockButton = document.querySelector(".button-rock");
+const paperButton = document.querySelector(".button-paper");
+const scissorsButton = document.querySelector(".button-scissors");
+const results = document.querySelector(".results");
+const playerScoreElement = document.querySelector(".score-player");
+const computerScoreElement = document.querySelector(".score-computer");
+
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
-  randomInput = Math.floor(Math.random() * choices.length);
+  const randomInput = Math.floor(Math.random() * choices.length);
   return choices[randomInput];
-}
-function getHumanChoice() {
-  return prompt("Rock, paper, or scissors?");
 }
 function playRound(humanChoice, computerChoice) {
   humanChoice = humanChoice.toLowerCase();
-  const matchups = new Map([
-    ["rock,rock", 0],
-    ["rock,paper", -1],
-    ["rock,scissors", 1],
-    ["paper,rock", 1],
-    ["paper,paper", 0],
-    ["paper,scissors", -1],
-    ["scissors,scissors", 0],
-    ["scissors,rock", -1],
-    ["scissors,paper", 1],
-  ]);
 
   const roundResult = document.createElement("p");
-  const results = document.querySelector(".results");
-  const playerScoreElement = document.querySelector(".score-player");
-  const computerScoreElement = document.querySelector(".score-computer");
 
-  if (matchups.get(`${humanChoice},${computerChoice}`) == 1) {
+  const outcome = matchups.get(`${humanChoice},${computerChoice}`);
+  if (outcome === 1) {
     humanScore++;
 
     roundResult.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
     playerScoreElement.textContent = `Player Score: ${humanScore}`;
-  } else if (matchups.get(`${humanChoice},${computerChoice}`) == 0) {
+  } else if (outcome === 0) {
     roundResult.textContent = `Tie! Y'all both chose ${humanChoice}`;
   } else {
     computerScore++;
@@ -43,8 +46,8 @@ function playRound(humanChoice, computerChoice) {
   results.appendChild(roundResult);
   console.log(humanScore);
 
-  if (humanScore == 5 || computerScore == 5) {
-    const winner = humanScore == 5 ? "You" : "Computer";
+  if (humanScore === POINTS_TO_WIN || computerScore === POINTS_TO_WIN) {
+    const winner = humanScore === POINTS_TO_WIN ? "You" : "Computer";
 
     const announceWinner = document.createElement("p");
     announceWinner.textContent = `${winner} won the game!`;
@@ -58,9 +61,6 @@ function playRound(humanChoice, computerChoice) {
 function playGame() {
   humanScore = 0;
   computerScore = 0;
-  const rockButton = document.querySelector(".button-rock");
-  const paperButton = document.querySelector(".button-paper");
-  const scissorsButton = document.querySelector(".button-scissors");
   rockButton.addEventListener("click", () =>
     playRound("rock", getComputerChoice())
   );
